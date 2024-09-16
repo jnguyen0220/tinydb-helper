1207d22f7cc1ff6c47cdeceefddf2ea9b42270bd
Jonny Nguyen <jonny_nguyen@outlook.com>
2024-09-15 23:24:53 -0500

    update readme

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def main() -> None:
     person = Table(db=db, table="person", key_gen=lambda: timestamp)
 
     for i in range(4):
-        project.insert(dict(name=f"test-{i}", year=f"{2004 + i}"))
-        person.insert(dict(first=f"first-{i}", year=f"{2004 + i}"))
-
+        project.insert(dict(name=f"test-{i}", year=f"{2004 + i}"), id=str(i))
+        person.insert(dict(first=f"first-{i}", year=f"{2004 + i}"), id=str(i))
+        
+    person.all()
+    person.find(id="0")
+    person.update(id="0", item=dict(test='pass'))
+    person.delete(id="0")
 
 if __name__ == "__main__":
     main()
