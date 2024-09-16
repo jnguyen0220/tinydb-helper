224baf5fe5415e2b07d60736a4c0505289eba2fa
Jonny Nguyen <jonny_nguyen@outlook.com>
2024-09-15 21:28:26 -0500

    add black and pre-commit hook

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -3,8 +3,9 @@ from tinydb_helper.helper import Table
 from tinydb import TinyDB
 from tinydb.storages import MemoryStorage
 
-TABLE = 'project'
-ID = '1'
+TABLE = "project"
+ID = "1"
+
 
 @pytest.fixture
 def table():
@@ -16,21 +17,25 @@ def test_all(table):
     table.insert(item=dict(), id=id)
     assert len(table.all()) == 1
 
+
 def test_insert(table):
     created_id = table.insert(item=dict(), id=ID)
     assert ID == created_id
+
 
 def test_update(table):
     table.insert(item=dict(), id=ID)
     update_id = table.update(id=ID, item=dict())
     assert ID == update_id
 
+
 def test_delete(table):
     table.insert(item=dict(), id=ID)
     delete_id = table.delete(id=ID)
     assert ID == delete_id
 
+
 def test_find(table):
     table.insert(item=dict(), id=ID)
     found = table.find(id=ID)
-    assert found != None
+    assert found is not None
